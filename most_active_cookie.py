edeeb03c4ccbd0fcfd80dd403076793baaecc8f1
aaronxie <aaron.xie2024@gmail.com>
2022-01-30 17:33:49 -0800

    add more comments

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -6,9 +6,10 @@ def readData(filename):
     allFiles = []
 
     with open(filename) as file:
+        # read csv file
         csv_reader = csv.DictReader(file, delimiter=",")
         for entry in csv_reader:
-            # O(1)
+            # 2d array for the csv contents. Each element has cookie name, cookie date, cookie time (from timestamp)
             cookieName = entry["cookie"]
             cookieDate = entry["timestamp"][0:10]
             cookieTime = entry['timestamp'][11:16]
@@ -18,22 +19,34 @@ def readData(filename):
 
 
 def closestDate(allCookies, tDate):
+    # cookies seen in loop thus far
     matchedCookies = {
         # cookieName: occur
     }
 
+    # track max occurrence thus far, and the corresponding cookie name in ans
     maxOccur = 0
     ans = ['N/A']
 
-    for cookieName, cookieDate in allCookies:
+    for cookieName, cookieDate in allCookies: # take the elements of the nested list
         if cookieDate == tDate:
+            # for the first match
             if maxOccur == 0:
                 maxOccur+=1;
                 ans = [cookieName]
+            
+            # for future matches
+
+            # if have seen the cookie already
             if cookieName in matchedCookies.keys():
                 matchedCookies[cookieName] += 1
+            # if first time seeing cookie
             else:
                 matchedCookies[cookieName] = 1
+
+
+            # for updating max occurrences
+
             if maxOccur < matchedCookies[cookieName]:
                 maxOccur = matchedCookies[cookieName]
                 ans = [cookieName]

5d859f1cc462599aa8706fc6f445bc79f07022ef
aaronxie <aaron.xie2024@gmail.com>
2022-01-30 17:39:29 -0800

    add test case

--- app_test.py
+++ app_test.py
@@ -50,6 +50,23 @@ class TestMostActiveCookie(unittest.TestCase):
             ans, ["SAZuXPGUrfbcn5UA", "4sMM2LxV07bPJzwf", "fbcn5UAVanZf6UtG"]
         )
 
+    def test_nullClosetDate(self):
+        sample_dates = [
+            ["AtY0laUfhglK3lC7", "2018-12-09"],
+            ["SAZuXPGUrfbcn5UA", "2018-12-09"],
+            ["5UAVanZf6UtGyKVS", "2018-12-09"],
+            ["AtY0laUfhglK3lC7", "2018-12-09"],
+            ["SAZuXPGUrfbcn5UA", "2018-12-08"],
+            ["4sMM2LxV07bPJzwf", "2018-12-08"],
+            ["fbcn5UAVanZf6UtG", "2018-12-08"],
+            ["4sMM2LxV07bPJzwf", "2018-12-07"],
+        ]
+
+        ans = closestDate(sample_dates, "2018-11-01")
+        self.assertCountEqual(
+            ans, ["N/A"]
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
